2c518778266ac2d1d6d998cd83efc8d26ef1af46
VadimZonder <vadimmalakhovski@hotmail.com>
2017-11-20 16:57:26 +0000

    SportPro

--- config/routes.rb
+++ config/routes.rb
@@ -43,5 +43,6 @@ Rails.application.routes.draw do
   
   get '/category/:title', to: 'static_pages#category'
 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
